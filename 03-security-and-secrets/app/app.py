3fd5b110707f82cb0832f97c1a667e8fff0ec36f
Sarvesh-N <sarveshnanthakumar99@gmail.com>
2026-05-26 10:09:16 +0530

    without security check

--- 03-security-and-secrets/app/app.py
+++ 03-security-and-secrets/app/app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    print("done")
     return "Application is running with Secrets Management..."
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
