acafa84f5179ed0979fac5f7dd6282c4d4ec4605
Sarvesh-N <sarveshnanthakumar99@gmail.com>
2026-05-26 10:18:18 +0530

    added docker logs monitoring

--- 03-security-and-secrets/app/app.py
+++ 03-security-and-secrets/app/app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    print("done")
+    print("added log viewer")
     return "Application is running with Secrets Management..."
+
 
 
 if __name__ == "__main__":
